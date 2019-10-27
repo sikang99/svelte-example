6a240e722948bab354c2147b8ea50cdb0671d3ac
Stoney Kang <sikang99@gmail.com>
2019-10-27 10:20:12 +0900

    00.00.01: update contents

--- Book-app-svelte/src/book.svelte
+++ Book-app-svelte/src/book.svelte
@@ -1,40 +1,34 @@
 <script>
-    import {createEventDispatcher} from 'svelte'
-    import Button from './button.svelte'
-    
-    export let bookTitle;
-    export let bookPages;
-    export let bookDescription;
+import {createEventDispatcher} from 'svelte'
+import Button from './button.svelte'
 
-    const dispatch =  createEventDispatcher()
-    function purchaseBook(){
-        dispatch('buy', bookTitle)
-    }
+export let bookTitle;
+export let bookPages;
+export let bookDescription;
 
+const dispatch =  createEventDispatcher()
+function purchaseBook(){
+    dispatch('buy', bookTitle)
+}
 </script>
 
 <style>
-    div{
-        margin: 1rem;
-        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26)
-    }
-
-    h1{
-        font-size: 1.25rem;
-        margin: 0.25rem 0;
-    }
-
-    h2{
-        font-size: 1rem;
-        margin: 0.25rem 0;
-        color: aqua;
-    }
-
-    p{
-        margin: 0.25rem 0;
-    }
-
-  
+div{
+    margin: 1rem;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26)
+}
+h1{
+    font-size: 1.25rem;
+    margin: 0.25rem 0;
+}
+h2{
+    font-size: 1rem;
+    margin: 0.25rem 0;
+    color: aqua;
+}
+p{
+    margin: 0.25rem 0;
+}
 </style>
 
 <div>
