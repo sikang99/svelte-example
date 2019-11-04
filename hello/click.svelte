0aa707d23a32391d310b44d4c3a2e1e72ed702b8
Stoney Kang <sikang99@gmail.com>
2019-11-04 14:02:41 +0900

    00.00.01: update contents

---
+++ hello/click.svelte
@@ -0,0 +1,18 @@
+<script>
+let count = 0
+let double = 0
+
+function handleClick() {
+    count += 1;
+}
+
+$: {
+    console.log(`the count is ${count}`)
+    double = count * 2
+    console.log(`double the count is ${double}`)
+}
+</script>
+
+<button on:click={handleClick}>
+    {double} by Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
